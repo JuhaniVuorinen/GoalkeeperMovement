cd8307f4d178b6dbc92b6a166087b1fb010614bf
JoskaT4 <josetolppala1@gmail.com>
2024-10-10 17:40:51 +0300

    ,
    ,

--- Assets/Scripts/PlayerAnimator.cs
+++ Assets/Scripts/PlayerAnimator.cs
@@ -6,20 +6,50 @@ public class PlayerAnimator : MonoBehaviour
 {
     public Animator animator;
 
+    public override bool Equals(object other)
+    {
+        return base.Equals(other);
+    }
+
+    public override int GetHashCode()
+    {
+        return base.GetHashCode();
+    }
+
+    public override string ToString()
+    {
+        return base.ToString();
+    }
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
+        Debug.Log(animator);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
-            {
+        if (Input.GetKeyDown("W"))
+        {
             animator.SetBool("IsSkating", true);
 
+            {
+                if (!Input.GetKeyDown("W"))
+                    animator.SetBool("IsMoving", false);
             }
-    return;    }
+
+
+            if (Input.GetKeyDown(KeyCode.Mouse0));
+
+            animator.SetBool("IsMoving", true);
+
+            if (!Input.GetKeyDown(KeyCode.Mouse0));
+
+            animator.SetBool("IsShooting", false);
+
+
+        }}
 }
